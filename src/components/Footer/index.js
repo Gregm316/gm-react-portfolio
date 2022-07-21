dc1a02462bf9057361ccfcb33965f0ce9194eb79
gregm316 <gregm316@gmail.com>
2022-07-21 01:13:50 -0400

    Added gmail link and icon to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,7 @@ function Footer() {
     <footer className="footer bg-dark fixed-bottom">
         <a href="https://github.com/Gregm316"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/gregory-maher-4b783b119/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="mailto:gregm316@gmail.com"><img src="https://img.icons8.com/color/48/000000/gmail-new.png" alt="Gmail" className="icon"/></a>
     </footer>
     )
 }
