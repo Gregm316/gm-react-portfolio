b86cf8c59514d87f49a17b7caef5bdf6951c7be2
gregm316 <gregm316@gmail.com>
2022-07-20 22:45:28 -0400

    Added resume function, linkedin link/icon and resume download link

---
+++ src/components/Resume/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Gregory Maher</h2>
+          <ul>
+            <li>
+            
+            </li>
+          </ul>
+
+          <p class="mt-5">
+            <a href="https://www.linkedin.com/in/gregory-maher-4b783b119/">
+              <img
+                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+                alt="inkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href=""
+            class="link"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title">Proficiencies</h2>
+          <ul>
+            <li>
+
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
